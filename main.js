cca3f5457e34730a498d4607f73b9621f6496944
watcharap <watchara.paramee@gmail.com>
2025-07-21 11:55:43 +0700

    Morning check point

--- main.js
+++ main.js
@@ -5,33 +5,67 @@ import clear from "clear-screen";
 
 const prompt = promptSync({ sigint: true });
 
-
 const hat = "^";
 const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
 
+// เกมส์นี้ต้องการให้ผู้เล่น"*"ตามหาหมวก"^"" จากจุดเริ่มต้นที่ 0 โดยมีอุปสรรคเป็นหลุม "0"
+// เริ่มต้น -> สร้าง field -> print field->  move function -> game conditions ->win or fault.
+
 class Field {
 	constructor(field = [[]]) {
 		this.field = field;
-
-		// Replace with your own code //
-		// Set the home position at (0, 0) before the game starts
 		this.positionRow = 0;
 		this.positionCol = 0;
+		this.gameOver = false;
 		this.field[this.positionRow][this.positionCol] = pathCharacter;
 	}
 
 	// Print field //
 	print() {
-		clear();
+		this.field.forEach(row => console.log (row.join('')));
 
-		// Replace with your own code //
-		console.log(this.field); // Please REMOVE this line before you start your code!
+		console.log(this.field);
 	}
 
-	// Your Code //
+	// move direction เงื่อนไขการเดินของผู้เล่น โดยการกำหนดการเคลื่อนที่ ซ้าย ขวา บน ล่าง และเงื่อนไขการเดินตกหลุม แล้ว Gameover 
+	move(direction){
+	let newX = this.playerX;
+	let newY = this.playerY;
+	if (direction === "up") newY-- ;
+	else if (direction === "down")newY++ ;
+	else if (direction === "left")newX-- ;
+	else if (direction === "right")newX++ ;
+	
+	//เงื่อนไข
+	if (
+		newY < 0 || newY > this.field.length ||
+		newX < 0 || newX > this.field.[0].length
+	){
+		console.log("Outside field / Game over");
+		this.gameOver = true;
+		return;
+	}
+
+	const walk = this.field[this.newX][this.newY];
+	if (walk === "0") {
+		console.log("Game over");
+		this.gameOver = true;
+	} else if(walk=== "^" ) {
+		console.log("Win");
+		this.gameOver=true;
+	} else {
+		this.playerX = newX;
+		this.playerY = newY;
+		this.field[this.newX][this.newY] = "*";
+		this.print();
+
+	}
+	}
 }
+
+
 
 // Game Mode ON
 // Remark: Code example below should be deleted and use your own code.
